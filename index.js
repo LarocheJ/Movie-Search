5ab5c6fd8b88b8ae4720dd21a8fa568efde1177e
LarocheJ <jimmy.lar91@gmail.com>
2020-03-05 20:44:14 -0700

    Changed port to 5000

--- index.js
+++ index.js
@@ -6,42 +6,32 @@ const bodyParser = require("body-parser");
 app.use(express.static(__dirname + "/public"));
 
 app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
+  bodyParser.urlencoded({
+    extended: true
+  })
 );
 
 app.set("view engine", "ejs");
 
-app.get("/get", (req, res, next) => {
-    res.json({
-        "version": "1.0"
-    });
-});
-
-app.post('/post', function (request, response) {
-    response.send(request.body);
-});
-
 app.get("/", (req, res) => {
-    res.render("search");
+  res.render("search");
 });
 
 app.get("/results", (req, res) => {
-    const query = req.query.search;
-    const url = `http://www.omdbapi.com/?s=${query}&apikey=thewdb`;
+  const query = req.query.search;
+  const url = `http://www.omdbapi.com/?s=${query}&apikey=thewdb`;
 
-    request(url, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            const data = JSON.parse(body);
-            res.render("results", {
-                data: data,
-                query: query
-            });
-        }
-    });
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.render("results", {
+        data: data,
+        query: query
+      });
+    }
+  });
 });
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
 });
